f71ac84c3fcc80275a4949979908635541dd5ad3
Elias Gill <eliasgill42@gmail.com>
2024-01-01 15:52:16 -0300

    fix: Fixing url validator from reader/utils

--- src/components/reader/utils.ts
+++ src/components/reader/utils.ts
@@ -2,17 +2,12 @@ import { marked } from "marked";
 import hljs from "highlight.js";
 import "highlight.js/styles/monokai.css";
 
-function isValidUrl(urlString: string): boolean {
-    const urlPattern = new RegExp(
-        "^(https?:\\/\\/)?" + // validate protocol
-            "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // validate domain name
-            "((\\d{1,3}\\.){3}\\d{1,3}))" + // validate OR ip (v4) address
-            "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // validate port and path
-            "(\\?[;&a-z\\d%_.~+=-]*)?" + // validate query string
-            "(\\#[-a-z\\d_]*)?$",
-        "i"
-    ); // validate fragment locator
-    return !!urlPattern.test(urlString);
+export function isUrl(url: string): boolean {
+    // the most mean solution you will ever see, but somehow works
+    if (url.includes("http") || url.includes("https") || url.includes("www")) {
+        return true;
+    }
+    return false;
 }
 
 // change links bahavior to open local files if the given href is not a web url
@@ -20,7 +15,7 @@ export function resolveLinks(changeFile: (s: string) => void) {
     document.querySelectorAll("a").forEach((element) => {
         // NOTE: must use "getAttribute"
         const link = element.getAttribute("href") as string;
-        if (!isValidUrl(link)) {
+        if (!isUrl(link)) {
             element.href = "#";
             element.addEventListener("click", function () {
                 changeFile(link);
